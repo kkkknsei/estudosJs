cfd402ac4aa5da1c3a35e0447b885e30b6e92952
kkkknsei <pcher1que1@gmail.com>
2021-08-23 23:26:19 -0300

    17th update

---
+++ node/aula9/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const route = require("./routes");
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(route);
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.listen(3000, () => {
+  console.log("Acessar http://localhost:3000");
+  console.log("Servidor executando na porta 3000");
+});
