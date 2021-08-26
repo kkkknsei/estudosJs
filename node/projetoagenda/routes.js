39cb7fd332b811a0d73577da61f543fba2adc2d0
kkkknsei <pcher1que1@gmail.com>
2021-08-26 19:07:49 -0300

    4th agenda update

--- node/projetoagenda/routes.js
+++ node/projetoagenda/routes.js
@@ -24,4 +24,8 @@ route.post("/contato/register", loginRequired, contatoController.register);
 
 route.get("/contato/index/:id", loginRequired, contatoController.editIndex);
 
+route.post("/contato/edit/:id", loginRequired, contatoController.edit);
+
+route.get("/contato/delete/:id", loginRequired, contatoController.delete);
+
 module.exports = route;
