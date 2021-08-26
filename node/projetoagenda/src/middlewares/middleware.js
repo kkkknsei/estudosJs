51d67a1a3bce20753cf8660f9387693be40d82a8
kkkknsei <pcher1que1@gmail.com>
2021-08-26 15:52:35 -0300

    2nd agenda update

--- node/projetoagenda/src/middlewares/middleware.js
+++ node/projetoagenda/src/middlewares/middleware.js
@@ -1,5 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = "Esté é o valor da variável local";
+  res.locals.errors = req.flash("errors");
+  res.locals.success = req.flash("success");
+  res.locals.user = req.session.user;
   next();
 };
 
@@ -17,5 +19,15 @@ exports.checkSrfError = (err, req, res, next) => {
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash("errors", "Você precisa estar logado!");
+    req.session.save(() => res.redirect("/"));
+    return;
+  }
+
   next();
 };
